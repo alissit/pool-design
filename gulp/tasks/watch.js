1346bee74cae81c8541791ab8cd963194ea4c015
Alissa <alissit@gmail.com>
2018-03-14 08:24:11 +0000

    save after crash

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,32 +1,31 @@
-var gulp			= require("gulp"),
-	watch			= require("gulp-watch"),
-	browserSync		= require("browser-sync").create();
-
+var gulp 			= require("gulp"), //brings in the gulp files
+	watch 			= require("gulp-watch"),//brings in gulp-watch
+	browserSync		= require("browser-sync").create();//only want the create aspect of browser-sync
 
 
 gulp.task("watch", function(){
 
-	browserSync.init({
-		notify: false,
-		server: {
-			baseDir: "app"
-		}
-	});
+		browserSync.init({
+			notify: false, /*stops the notify box from showing up*/
+			server:{
+				baseDir: "app"
+			}
+		});
 
-	watch("./app/index.html", function(){
-		console.log("watching html")
-		browserSync.reload();
-	});
+		watch("./app/index.html", function(){
+			console.log("watching html")
+			browserSync.reload();
+		});
 
-	watch("./app/assets/styles/**/*.css", function(){
-		console.log("watching styles")
-		gulp.start("cssInject");	
-	});
+		watch("./app/assets/styles/**/*.css", function(){
+			console.log("watching styles")
+			gulp.start("cssInject");
+		});
 
-	watch("./app/assets/scripts/**/*.js", function(){
-		console.log("watching javascript")
-		gulp.start("scriptsRefresh");
-	});
+		watch("./app/assets/scripts/App.js", function(){
+			console.log("watching scripts")
+			gulp.start("scriptsRefresh");
+		});
 });
 
 gulp.task("cssInject", ["styles"], function(){
@@ -34,7 +33,6 @@ gulp.task("cssInject", ["styles"], function(){
 		.pipe(browserSync.stream());
 });
 
-gulp.task("scriptsRefresh", ["scripts"], function(){
-	browserSync.reload();
+gulp.task("scriptsRefresh",["scripts"], function(){
+		browserSync.reload();
 });
-
