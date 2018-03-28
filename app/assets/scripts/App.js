c1ab7e025fbac95cd8ac2e70dd4d28b5d74018a7
Alissa <alissit@gmail.com>
2018-03-28 11:52:42 +0100

    finished

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,11 @@
 import MobileMenu from "./modules/MobileMenu";
 import Gallery from "./modules/Gallery";
 import $ from "jquery";
+import RevealOnScroll from "./modules/RevealOnScroll";
 import Lightbox2 from "../../../node_modules/lightbox2/dist/js/lightbox";
 
 
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".icons"), "60%");
+new RevealOnScroll($(".foot-slidera"), "60%");
